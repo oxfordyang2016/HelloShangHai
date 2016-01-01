46a44ca3e56006dc7e1e8e0f14d0f93fe4c944db
oxfordyang2016 <yang756260386@gmail.com>
2016-01-01 20:59:44 +0800

    类方法的说明

--- src/shanghaitest/Student.java
+++ src/shanghaitest/Student.java
@@ -4,7 +4,7 @@ public class Student {
 	public static void main(String []args){
 		Fee fee1=new Fee(15,"yang",345);
 		Fee fee2=new Fee(17,"ming",643);
-		System.out.println(fee2.gettotalfee());
+		System.out.println(Fee.gettotalfee());
 	}
 
 
@@ -22,7 +22,8 @@ class Fee{
 		totalfee+=fee;
 	}
 	//返回总学费
-	public int gettotalfee(){
+	public static int gettotalfee(){//这里加入static后了就变成了类方法
+		//类变量和静态变量原则上用类方法去访问和操作，类方法中不能访问非静态变量；
 	return totalfee;	
 	} 
 }
