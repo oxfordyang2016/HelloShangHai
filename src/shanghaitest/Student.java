1fd31ffbd27ea10004f7d51f07d4556d77a8e3a0
oxfordyang2016 <yang756260386@gmail.com>
2016-01-01 21:03:10 +0800

    类方法

--- src/shanghaitest/Student.java
+++ src/shanghaitest/Student.java
@@ -5,6 +5,7 @@ public class Student {
 		Fee fee1=new Fee(15,"yang",345);
 		Fee fee2=new Fee(17,"ming",643);
 		System.out.println(Fee.gettotalfee());
+		System.out.println(fee2.gettotalfee());//这里的两句都是一个效果
 	}
 
 
